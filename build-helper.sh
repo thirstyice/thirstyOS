c01d55bc2fff841615432ace18b3dba18623b42e
thirstyice <tauranwood@gmail.com>
2017-11-04 20:07:49 -0600

    Made device selection easier in build-helper.sh

--- build-helper.sh
+++ build-helper.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 
-## This assumes that your usb is /dev/sdc
+echo "Device:"
+read device
+
 git pull --ff-only
 if [[ $? == 0 ]]; then
 echo "Version number:"
@@ -13,31 +15,28 @@ DISTRIB_CODENAME=$version">config/includes.chroot/etc/lsb-release
 git add config/includes.chroot/etc/lsb-release
 git commit -m "Updated version number in lsb-release to $version"
 fi
-sudo su -s /bin/bash -c '
-echo "
+sudo su -s /bin/bash -c "
+echo \"
 
-configuring"
-echo -e "\e[96m"
+configuring\"
+echo -e \"\e[96m\"
 lb config --verbose;
-echo -e "\e[0m
+echo -e \"\e[0m
 config done, building
-\e[95m"; 
+\e[95m\"; 
 lb build --verbose; 
-echo -e "\e[0m
+echo -e \"\e[0m
 build done, clearing usb
-\e[94m"; 
-dd if=/dev/zero of=/dev/sdc bs=4M;
+\e[94m\"; 
+dd if=/dev/zero of=$device bs=4M;
 sync;
-echo -e "\e[0m
+echo -e \"\e[0m
 cleared, copying iso
-\e[93m"; 
-dd if=live-image-i386.hybrid.iso of=/dev/sdc bs=4M; 
+\e[93m\"; 
+dd if=live-image-i386.hybrid.iso of=$device bs=4M; 
 sync;
-echo -e "\e[0m
-copied, cleanng
-\e[92m";
-lb clean;
-'
+echo -e \"\e[0m
+"
 
 else
 echo "Git pull failed; Merge then try again"
