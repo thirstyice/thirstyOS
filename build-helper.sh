51202641f706d960f41a1e829c4e1a9108570c32
thirstyice <tauranwood@gmail.com>
2020-03-31 21:21:02 -0600

    Build-helper: made optimizations to readability and functionality

--- build-helper.sh
+++ build-helper.sh
@@ -1,29 +1,33 @@
-#/bin/sh
+#!/bin/bash
 
+if [ $(id -u) -ne 0 ]; then
+	sudo $0 $@
+	exit
+fi
 echo "Device:"
 read device
-
-sudo su -s /bin/bash -c "
-echo \"
-
-configuring\"
-echo -e \"\e[96m\"
-lb config --verbose;
-echo -e \"\e[0m
+echo -e "clearing usb in background"
+dd if=/dev/zero of=$device bs=4M &
+pid=$!
+echo "pid is $pid"
+echo -e "\e[0m
+configuring
+\e[96m"
+lb config --verbose
+echo -e "\e[0m
 config done, building
-\e[95m\"; 
-lb build --verbose; 
-echo -e \"\e[0m
-build done, clearing usb
-\e[94m\"; 
-dd if=/dev/zero of=$device bs=4M;
-sync;
-echo -e \"\e[0m
-cleared, copying iso
-\e[93m\"; 
-dd if=live-image-amd64.hybrid.iso of=$device bs=4M; 
-sync;
-echo -e \"\e[0m
+\e[95m"
+lb build --verbose
+echo -e "\e[0m
+build done, verifying usb cleared
+\e[94m"
+wait $pid
+sync
+echo -e "\e[0m
+usb cleared, copying iso
+\e[93m"
+dd if=live-image-amd64.hybrid.iso of=$device bs=4M
+sync
+echo -e "\e[0m
 done!
-\";
 "
